9fc22c11197486f3085552fec17d1495840574e8
Ashutosh Mehta <93917407+ashumehta26@users.noreply.github.com>
2022-03-02 18:25:36 +0530

    bsc debug

--- eth/backend.go
+++ eth/backend.go
@@ -464,6 +464,7 @@ func (s *Ethereum) SetEtherbase(etherbase common.Address) {
 // is already running, this method adjust the number of threads allowed to use
 // and updates the minimum price required by the transaction pool.
 func (s *Ethereum) StartMining(threads int) error {
+	fmt.Println("inside StartMining() in backend.go")
 	// Update the thread count within the consensus engine
 	type threaded interface {
 		SetThreads(threads int)
